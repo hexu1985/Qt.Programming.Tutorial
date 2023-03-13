9c16821ff5add912f6c0ba29984f2c67bae9a2cb
hexu <hexu_1985@sina.com>
2023-03-13 15:45:32 +0800

    update creating_dialog/sort_src

--- creating_dialog/sort_src/sortdialog.h
+++ creating_dialog/sort_src/sortdialog.h
@@ -2,14 +2,24 @@
 #define SORTDIALOG_H
 
 #include <QDialog>
+#include <QComboBox>
 
 class SortDialog : public QDialog
 {
     Q_OBJECT
 
+private:
+    QComboBox* primaryColumnCombo; 
+    QComboBox* primaryOrderCombo; 
+    QComboBox* secondaryColumnCombo; 
+    QComboBox* secondaryOrderCombo; 
+    QComboBox* tertiaryColumnCombo; 
+    QComboBox* tertiaryOrderCombo; 
+
 public:
     SortDialog(QWidget *parent = 0);
 
+    void setOrderOption();
     void setColumnRange(QChar first, QChar last);
 };
 
